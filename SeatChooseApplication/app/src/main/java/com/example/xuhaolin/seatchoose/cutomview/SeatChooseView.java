407d011a4c798f2a05a45b32e7f587956c59e22c
徐浩霖 <xuhaolin@xuhaolindeMac-mini.local>
2015-08-14 17:02:21 +0800

    修改部分测试数据

--- SeatChooseApplication/app/src/main/java/com/example/xuhaolin/seatchoose/cutomview/SeatChooseView.java
+++ SeatChooseApplication/app/src/main/java/com/example/xuhaolin/seatchoose/cutomview/SeatChooseView.java
@@ -4,8 +4,11 @@ package com.example.xuhaolin.seatchoose.cutomview;/**
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.example.xuhaolin.seatchoose.R;
 
 /**
  * Author:
@@ -26,11 +29,16 @@ public class SeatChooseView extends View {
 
     public void initial() {
         mSeatDrawUtils = SeatDrawUtils.getInstance(getContext(), this);
-        SeatParams params = SeatParams.getInstance();
-        params.setSeatHeight(50f);
-        params.setSeatWidth(60f);
-        mSeatDrawUtils.setSeatParams(params);
+        StageParams stage = StageParams.getInstance();
+        stage.setStageImage(R.drawable.icon_bg);
+        stage.setStageTextColor(Color.WHITE);
+        mSeatDrawUtils.setStageParams(stage);
 
+        SeatParams seat = SeatParams.getInstance();
+        seat.setSeatHeight(50f);
+        seat.setSeatWidth(50f);
+        seat.setSeatImage(new int[]{R.drawable.icon_logo_main, R.drawable.icon_logo_pkq, R.drawable.icon_logo_alpaca});
+        mSeatDrawUtils.setSeatParams(seat);
     }
 
     @Override
