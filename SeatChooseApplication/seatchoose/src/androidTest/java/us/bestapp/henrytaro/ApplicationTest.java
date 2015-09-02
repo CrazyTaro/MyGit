2118380b1953abb4069b522707564be88c5f0926
徐浩霖 <xuhaolin@xuhaolindeMac-mini.local>
2015-09-02 17:51:50 +0800

    新增测试方法

--- SeatChooseApplication/seatchoose/src/androidTest/java/us/bestapp/henrytaro/ApplicationTest.java
+++ SeatChooseApplication/seatchoose/src/androidTest/java/us/bestapp/henrytaro/ApplicationTest.java
@@ -2,6 +2,11 @@ package us.bestapp.henrytaro;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.util.Log;
+
+import java.util.Arrays;
+
+import us.bestapp.henrytaro.entity.SeatMap;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -10,4 +15,85 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    public void testParseJsonStr() {
+//            String str =
+//                    "{\"rownum\": \"1\", \n\"rowid\": \"1\", \n\"columns\": \"ZL,01@A@0,02@A@0,03@A@0,04@A@0,05@A@0,06@A@0,07@A@0,08@A@0,09@A@0,10@A@0,11@A@0,12@A@0\"\n}";
+//            Gson gson = new Gson();
+//            Row row = gson.fromJson(str, Row.class);
+//            String rowStr = row.toString();
+
+        String jsonStr = "{\n" +
+                "    \"success\": true, \n" +
+                "    \"error_code\": \"0\", \n" +
+                "    \"message\": \"请求成功\", \n" +
+                "    \"data\": {\n" +
+                "        \"row\": [\n" +
+                "            {\n" +
+                "                \"rownum\": \"1\", \n" +
+                "                \"rowid\": \"1\", \n" +
+                "                \"columns\": \"ZL,01@A@0,02@A@0,03@A@0,04@A@0,05@A@0,06@A@0,07@A@0,08@A@0,09@A@0,10@A@0,11@A@0,12@A@0\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"rownum\": \"2\", \n" +
+                "                \"rowid\": \"2\", \n" +
+                "                \"columns\": \"ZL,01@A@0,02@A@0,03@A@0,04@A@0,05@A@0,06@A@0,07@A@0,08@A@0,09@A@0,10@A@0,11@A@0,12@A@0\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"rownum\": \"3\", \n" +
+                "                \"rowid\": \"3\", \n" +
+                "                \"columns\": \"ZL,01@A@0,02@A@0,03@A@0,04@A@0,05@A@0,06@A@0,07@A@0,08@A@0,09@A@0,10@A@0,11@A@0,12@A@0\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"rownum\": \"4\", \n" +
+                "                \"rowid\": \"4\", \n" +
+                "                \"columns\": \"ZL,01@A@0,02@A@0,03@A@0,04@A@0,05@A@0,06@A@0,07@A@0,08@A@0,09@A@0,10@A@0,11@A@0,12@A@0\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"rownum\": \"5\", \n" +
+                "                \"rowid\": \"5\", \n" +
+                "                \"columns\": \"ZL,01@A@0,02@A@0,03@A@0,04@A@0,05@A@0,06@A@0,07@A@0,08@A@0,09@A@0,10@A@0,11@A@0,12@A@0\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"rownum\": \"6\", \n" +
+                "                \"rowid\": \"6\", \n" +
+                "                \"columns\": \"ZL,ZL,ZL,01@A@0,02@A@0,03@A@0,04@LK@0,05@A@0,06@A@0,07@A@0,08@A@0,09@A@0,10@A@0\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"rownum\": \"7\", \n" +
+                "                \"rowid\": \"7\", \n" +
+                "                \"columns\": \"ZL,ZL,ZL,01@A@0,02@A@0,03@A@0,04@A@0,05@A@0,06@A@0,07@A@0,08@A@0,09@A@0,10@A@0\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"rownum\": \"8\", \n" +
+                "                \"rowid\": \"8\", \n" +
+                "                \"columns\": \"ZL,ZL,ZL,01@A@0,02@LK@0,03@LK@0,04@A@0,05@A@0,06@LK@0,07@A@0,08@A@0,09@A@0,10@A@0\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"rownum\": \"9\", \n" +
+                "                \"rowid\": \"9\", \n" +
+                "                \"columns\": \"ZL,ZL,ZL,01@A@0,02@A@0,03@A@0,04@A@0,05@A@0,06@A@0,07@A@0,08@A@0,09@A@0,10@A@0\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"rownum\": \"10\", \n" +
+                "                \"rowid\": \"10\", \n" +
+                "                \"columns\": \"01@A@0,02@A@0,ZL,03@A@0,04@A@0,05@A@0,06@A@0,07@LK@0,08@LK@0,09@LK@0,10@A@0,11@A@0,12@A@0\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"rownum\": \"11\", \n" +
+                "                \"rowid\": \"11\", \n" +
+                "                \"columns\": \"01@A@0,02@A@0,03@A@0,04@A@0,05@A@0,06@A@0,07@A@0,08@A@0,09@A@0,10@A@0,11@A@0,12@A@0,13@A@0\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    }, \n" +
+                "    \"source\": \"gewala\"\n" +
+                "}";
+
+        SeatMap map = SeatMap.objectFromJSONStr(jsonStr);
+        int[][] drawMap = SeatMap.getDrawMap(map);
+        for (int i = 0; i < drawMap.length; i++) {
+            Log.i("map_" + (i + 1), Arrays.toString(drawMap[i]));
+        }
+    }
+
 }
