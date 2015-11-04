53f1d901b1c50e685a0bce6d0e992db55a59d304
徐浩霖 <xuhaolin@xuhaolindeMac-mini.local>
2015-11-04 18:43:50 +0800

    添加新接口（暂用）

---
+++ TestApplication/app/src/main/java/us/bestapp/henrytaro/player/interfaces/IAblumUpdateCallBack.java
@@ -0,0 +1,12 @@
+package us.bestapp.henrytaro.player.interfaces;
+
+import android.graphics.Bitmap;
+
+import us.bestapp.henrytaro.player.model.AbsTrack;
+
+/**
+ * Created by xuhaolin on 15/11/4.
+ */
+public interface IAblumUpdateCallBack {
+    public void updateAblumn(Bitmap ablumn,AbsTrack updateTrack);
+}
