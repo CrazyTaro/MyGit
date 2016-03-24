16c6da93a77979fd97a12a34b4b88e0ffcbd02c5
henry <henry@bestapp.us>
2016-03-24 11:21:33 +0800

    删除testApplication

--- TestApplication/app/src/main/java/us/bestapp/henrytaro/player/interfaces/IServiceConnection.java
+++
@@ -1,9 +0,0 @@
-package us.bestapp.henrytaro.player.interfaces;
-
-/**
- * Created by xuhaolin on 15/11/3.
- */
-public interface IServiceConnection {
-    public void onServiceConnected(ITrackHandleBinder binder);
-    public void onServiceDisconnected();
-}
